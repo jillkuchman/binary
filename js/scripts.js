ce6aba822fe4348bb73c0e668caa55351c41de15
Conor Baumgart & Jill Kuchman <student@epicodus.com>
2015-04-09 15:03:45 -0700

    Add final touches

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,43 @@
 function binary(input) {
-    var input_array = input.toString().split("");
+    var input_array = input.split("");
     var reversed_array = input_array.reverse();
     var counter = 0;
+    var index = 0;
     reversed_array.forEach(function(digit) {
-        var added = parseInt(digit)*Math.pow(2, reversed_array.indexOf(digit));
+        var added = (parseInt(digit))*(Math.pow(2, index));
         counter += added;
+        index ++;
     });
     return counter;
-}
+};
+
+$(document).ready(function() {
+    $("form#binary").submit(function(event){
+
+        var input = $("input#user_input").val();
+        var result = binary(input);
+
+        var user_input_array = input.split("");
+        var binary_check = true;
+        user_input_array.forEach(function(check_digit) {
+            if (check_digit != 1 && check_digit != 0) {
+                binary_check = false;
+            }
+        });
+
+        if (binary_check) {
+            $("#result_binary_true").show();
+            $(".input_binary").text(input);
+            $(".result_decimal").text(result);
+            $("#not_binary").hide();
+        }
+        else {
+            $("#not_binary").show();
+            $(".not_binary").text("Please enter a binary number");
+            $("#result_binary_true").hide();
+        }
+
+
+        event.preventDefault();
+    });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,20 @@
 describe('binary', function() {
     it("will return 0 if the user submits 0", function() {
-        expect(binary(0)).to.equal(0);
+        expect(binary("0")).to.equal(0);
     });
     it("will return 1 if the user submits 1", function() {
-        expect(binary(1)).to.equal(1);
+        expect(binary("1")).to.equal(1);
     });
     it("will return 2 if the user submits 10", function() {
-        expect(binary(101010001011)).to.equal(2);
+        expect(binary("10")).to.equal(2);
+    });
+    it("will return 3 if the user submits 11", function() {
+        expect(binary("11")).to.equal(3);
+    });
+    it("will return 853 if the user submits 1101010101", function() {
+        expect(binary("1101010101")).to.equal(853);
+    });it("will return if the user submits 111111111101010111010001010100101111010100010101101011", function() {
+        expect(binary("111111111111111111111")).to.equal(2097151
+        );
     });
 });
