f3597b712731c109e9118af5a8677fc040842aa8
Conor Baumgart & Jill Kuchman <student@epicodus.com>
2015-04-09 12:19:31 -0700

    Add test and method for numbers greater than 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 function binary(input) {
-    return input;
+    var input_array = input.toString().split("");
+    var reversed_array = input_array.reverse();
+    var counter = 0;
+    reversed_array.forEach(function(digit) {
+        var added = parseInt(digit)*Math.pow(2, reversed_array.indexOf(digit));
+        counter += added;
+    });
+    return counter;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,10 @@ describe('binary', function() {
     it("will return 0 if the user submits 0", function() {
         expect(binary(0)).to.equal(0);
     });
+    it("will return 1 if the user submits 1", function() {
+        expect(binary(1)).to.equal(1);
+    });
+    it("will return 2 if the user submits 10", function() {
+        expect(binary(101010001011)).to.equal(2);
+    });
 });
